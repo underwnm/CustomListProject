d8920abc0b0fecb04218893063f6707daf737324
Nicholas Underwood <underwnm@gmail.com>
2016-11-01 12:08:19 -0500

    Worked on methods
    Added Add.
    Added Remove.
    Added RemoveAt.
    Added IndexOf.
    Added Count.
    Added ReturnAt.
    Added override ToString.
    Added Zipper

---
+++ CustomListProject/CustomList.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListProject
+{
+    public class CustomList<T>
+
+    {
+        private int size;
+        private int buffer;
+        private T[] items;
+        public CustomList()
+        {
+            size = 0;
+            buffer = 8;
+            items = new T[buffer];
+        }
+        public void Add(T item)
+        {
+            T[] tempItems = new T[size+buffer];
+            for (int i = 0; i < size; i++)
+            {
+                tempItems[i] = items[i];
+            }
+            tempItems[size] = item;
+            items = tempItems;
+            size++;
+        }
+        public int IndexOf(T item)
+        {
+            int index = -1;
+            for (int i = 0; i < size; i++)
+            {
+                if (items[i].Equals(item))
+                    index = i;
+            }
+            return index;
+        }
+        public void Remove(T item)
+        {
+            int index = IndexOf(item);
+            while (index >= 0)
+            {
+                RemoveAt(index);
+                index = IndexOf(item);
+            }
+        }
+        public void RemoveAt(int index)
+        {
+            if(index >= 0 && index < size)
+            {
+                for (int i = index; i < size; i++)
+                {
+                    items[i] = items[i + 1];             
+                }
+                size--;
+            }
+        }
+        public int Count()
+        {
+            return size;
+        }
+        public T ReturnAt(int index)
+        {
+            return items[index];
+        }
+        public override string ToString()
+        {
+            StringBuilder itemsStringList = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                itemsStringList.Append(items[i]);
+            }
+            return itemsStringList.ToString();
+        }
+        public void Zipper(CustomList<T> List2)
+        {
+            T[] tempItems = new T[size + List2.size + buffer];
+            if (size >= List2.size)
+            {
+                for (int i = 0; i < List2.size; i++)
+                {
+                    tempItems[i*2] = items[i];
+                    tempItems[(i*2)+1] = List2.items[i];
+                }
+                for (int i = List2.size*2; i < size+List2.size; i++)
+                {
+                    tempItems[i] = items[i-List2.size];
+                }
+            } 
+            else if (List2.size > size)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    tempItems[i*2] = items[i];
+                    tempItems[(i*2)+1] = List2.items[i];
+                }
+                for (int i = size*2; i < List2.size+size; i++)
+                {
+                    tempItems[i] = List2.items[i-size];
+                }
+            }
+            items = tempItems;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AddIntItemToList()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            int value1 = customList.ReturnAt(0);
+            int value2 = customList.ReturnAt(1);
+            int value3 = customList.ReturnAt(2);
+            Assert.AreEqual(1, value1);
+            Assert.AreEqual(2, value2);
+            Assert.AreEqual(3, value3);
+        }
+        [TestMethod]
+        public void RemoveAllOfItemInList()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(2);
+            customList.Add(2);
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(2);
+            customList.Add(2);
+            customList.Remove(2);
+            Assert.AreEqual(1, customList.ReturnAt(0));
+        }
+        [TestMethod]
+        public void RemoveAtIndexInList()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            customList.RemoveAt(0);
+            Assert.AreEqual(2, customList.ReturnAt(0));
+        }
+        [TestMethod]
+        public void GetCountOfList()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            Assert.AreEqual(3, customList.Count());
+        }
+        [TestMethod]
+        public void GetListToString()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            customList.Add(1);
+            customList.Add(2);
+            customList.Add(3);
+            Assert.AreEqual("123", customList.ToString());
+        }
+        [TestMethod]
+        public void ZipperTwoLists()
+        {
+            CustomList<int> customList = new CustomList<int>();
+            CustomList<int> customList2 = new CustomList<int>();
+            customList.Add(1);
+            customList.Add(3);
+            customList.Add(5);
+            customList.Add(7);
+            customList2.Add(2);
+            customList2.Add(4);
+            customList2.Add(6);
+            customList2.Add(8);
+            customList2.Add(9);
+            customList.Zipper(customList2);
+            Assert.AreEqual(1, customList.ReturnAt(0));
+            Assert.AreEqual(2, customList.ReturnAt(1));
+            Assert.AreEqual(3, customList.ReturnAt(2));
+            Assert.AreEqual(4, customList.ReturnAt(3));
+            Assert.AreEqual(5, customList.ReturnAt(4));
+            Assert.AreEqual(6, customList.ReturnAt(5));
+            Assert.AreEqual(7, customList.ReturnAt(6));
+            Assert.AreEqual(8, customList.ReturnAt(7));
+            Assert.AreEqual(9, customList.ReturnAt(8));
+        }
+    }
+}
